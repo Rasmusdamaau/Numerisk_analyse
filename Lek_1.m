796b02216cbc327c997ec44e944b824fbf4a569c
Rasmusdamaau <Rasmus72dam@gmail.com>
2021-09-27 14:50:51 +0200

    første par opgaver lek 4

--- Lek_1.m
+++ Lek_1.m
@@ -48,11 +48,77 @@
 
 % Lek 2:
 
-A = [0 1 3 ; 2 5 1 ; 1 1 1];
-A = [4 3 7 ; -4 -1 -3 ; 2 1 2]
+% A = [0 1 3 ; 2 5 1 ; 1 1 1];
+% A = [4 3 7 ; -4 -1 -3 ; 2 1 2]
 
-[L, U, P] = lu(A)
+% [L, U, P] = lu(A)
+
+% Lek 3:
+
+% opg 2.9
+% A = [ 1 2 3 ; 4 5 6 ; 7 8 9 ];
+% b = [1 3 5]';
+
+% A\b;
+
+% opg. 2.6
+% n = 1;
+% A = 2*eye(n) + -tril(ones(n));
+% while cond(A,1) <= 1/eps 
+%     A = 2*eye(n) + -tril(ones(n));
+%     n * 2^(n-1);
+%     cond(A,1);
+%         
+%     n = n+1
+% end
+
+
+
+% unif_fordel = 100;
+% n = 48;
+% A = 2*eye(n) + -tril(ones(n));
+% x = -unif_fordel + (unif_fordel+unif_fordel)*rand([n 1]);
+% while norm(A*x) >= norm(x) 
+%     
+%     x = -unif_fordel + (unif_fordel+unif_fordel)*rand([n 1]);
+%     norm(A*x)
+%     norm(x)
+% 
+% end
+
+
+% Lek 4
+
+% opg. 2
+% N = 50;
+% 
+% x = cos((1:N)*pi/N);
+% y = 1./(1 + 25*x.^2);
+% 
+% interpgui(x,y);
+% polynomial works well
+
+
+% opg. 4
+
+n = 100;
+over = diag(repmat(-1,1,n-1),1);
+diago = diag(repmat(2,1,n),0);
+under = diag(repmat(-1,1,n-1),-1);
+b = (1:n)';
+
+mat = over + diago + under;
+
+mat\b
 
 
 
 
+
+
+
+
+
+
+
+    
