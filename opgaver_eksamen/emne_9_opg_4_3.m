b75b7125cd24d2f4c06630b0ae61de1d273c4a5b
Rasmusdamaau <Rasmus72dam@gmail.com>
2022-01-12 21:10:31 +0100

    emne 9 opg 4.3

---
+++ opgaver_eksamen/emne_9_opg_4_3.m
@@ -0,0 +1,63 @@
+
+p = @(x) 816*x.^3 - 3835*x.^2 + 6000*x - 3125;
+pprime = @(x) 3*816*x.^2 - 2*3835*x + 6000;
+
+%% a
+poly = [816 -3835 6000 -3125];
+r = roots(poly)
+
+%% b
+
+fplot(p,[1.43,1.71]);
+hold on, plot(r,zeros(3,1),'o');
+hold off;
+
+%% c
+
+xprev = 0;
+x = 1.5;
+k = 0;
+while abs(x - xprev) > eps*abs(x)
+    xprev = x;
+    x = x - p(x)/pprime(x)
+    k = k + 1;
+end
+k
+
+%% d
+
+a=1;
+b=2;
+
+while abs(b-a) > eps*abs(b)
+    c = a;
+    a = b;
+    b = b + (b - c)/(p(c)/p(b)-1)
+    k = k + 1;
+end
+
+k
+
+%% e
+
+a = 1;
+b = 2;
+k = 0;
+while abs(b-a) > eps*abs(b)
+    x = (a + b)/2;
+    if sign(p(x)) == sign(p(b))
+        b = x;
+    else
+        a = x;
+    end
+    k = k + 1;
+end
+a
+k
+
+
+%% f
+
+fzerotx(p, [1,2])
+
+
