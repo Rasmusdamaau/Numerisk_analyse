13bbedb5286b854ef62c61ffad0b3c104e377b8a
Rasmusdamaau <rasmus72dam@gmail.com>
2022-01-25 12:26:46 +0100

    asd

--- opgaver_eksamen/emne_2_opg_4_11.m
+++ opgaver_eksamen/emne_2_opg_4_11.m
@@ -1,5 +1,10 @@
 %% opg. 4.11
 
+% What is the largest value of n for which
+% Gamma(n + 1) and n! 
+% can be exactly represented 
+% by a double-precision floating-point number?
+
 n = 1;
 while abs(gamma(n + 1) - factorial(n)) == 0
     n = n +1;
@@ -11,6 +16,12 @@ gamma(n + 1) - factorial(n)
 gamma(n + 1)
 factorial(n)
 %%
+
+% What is the largest value of n for which
+% Gamma(n+1) and n! can be approximately
+% represented by a double precision floating-point
+% number that does not overflow?
+
 n = 1;
 while gamma(n + 1) ~= inf
     n = n + 1;
